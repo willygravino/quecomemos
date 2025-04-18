7600a7054e0c248e43867acff3f81dd3518529c8
ggravino <ggravino@gmail.com>
2025-04-18 18:34:30 -0300

    Preparar proyecto para deploy en Railway 2

--- nuestrotubo/settings.py
+++ nuestrotubo/settings.py
@@ -23,10 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ff(ii27o)5bnj*cy5l6bz1hr=hecla%@rxat^zg8g*&6z&e1dx'
+# SECRET_KEY = 'django-insecure-ff(ii27o)5bnj*cy5l6bz1hr=hecla%@rxat^zg8g*&6z&e1dx'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ff(ii27o)5bnj*cy5l6bz1hr=hecla%@rxat^zg8g*&6z&e1dx')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = False
+
 
 # ALLOWED_HOSTS = []
 
