8ea17db1a55bf9b4297faf792a424ef8ecdfcb81
ggravino <ggravino@gmail.com>
2025-04-18 19:16:09 -0300

    Actualizo requirements.txt 3

--- nuestrotubo/settings.py
+++ nuestrotubo/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ff(ii27o)5bnj*cy5l6bz
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = False
+# DEBUG = False
+
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
 # ALLOWED_HOSTS = []
