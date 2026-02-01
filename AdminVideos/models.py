7f6f55567c59921149878bb786d9949d0090d997
ggravino <ggravino@gmail.com>
2026-02-01 09:17:49 -0300

    vuelta a cascada en modo delete: cuando se borra un plato, si está programado, borra tambien los programados

--- AdminVideos/models.py
+++ AdminVideos/models.py
@@ -369,7 +369,7 @@ class MenuItem(models.Model):
     plato = models.ForeignKey("Plato", null=True, blank=True,
                             on_delete=models.PROTECT, related_name="en_menus")
     lugar = models.ForeignKey("Lugar", null=True, blank=True,
-                          on_delete=models.PROTECT, related_name="en_menus")
+                          on_delete=models.CASCADE, related_name="en_menus")
 
     # plato = models.ForeignKey("Plato", null=True, blank=True, on_delete=models.CASCADE, related_name="en_menus")
     # lugar = models.ForeignKey("Lugar", null=True, blank=True, on_delete=models.CASCADE, related_name="en_menus")
